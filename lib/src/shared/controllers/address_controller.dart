acb88da3030a35a3274a69c5bd7435566bfdd5cf
Rafael Claumann Bernardes <rafael_claumann@hotmail.com>
2026-04-11 15:58:38 -0300

    feat: implementar busca real de CEP via ViaCepService no AddressController
    - Sobrescreve o método `toString` em `AddressModel` para facilitar o debugging.
    - Integra o `ViaCepService` no `AddressController` para buscar endereços reais em vez de utilizar dados mockados.
    - Adiciona gerenciamento de estado (`isLoading`) e tratamento de exceções com logs (utilizando o pacote `logger`) durante a adição de novos endereços.
    - Garante a chamada de `notifyListeners()` após a conclusão da busca para atualizar a interface do usuário.

--- lib/src/shared/controllers/address_controller.dart
+++ lib/src/shared/controllers/address_controller.dart
@@ -1,15 +1,24 @@
+import 'package:fast_location/src/modules/home/services/viacep_service.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 
 import '../../modules/home/repositories/address_repository.dart';
 import '../models/address_model.dart';
+
+var logger = Logger();
 
 // O ChangeNotifier permite que as telas "escutem" quando os dados mudarem
 class AddressController extends ChangeNotifier {
   final AddressRepository _repository = AddressRepository();
 
+  final ViaCepService _viaCepService = ViaCepService();
+
   final List<AddressModel> _staticAddresses = [
     AddressModel(cep: '01001-000', address: 'Praça da Sé, Sé, São Paulo - SP'),
-    AddressModel(cep: '20040-002', address: 'Avenida Rio Branco, Rio de Janeiro - RJ',),
+    AddressModel(
+      cep: '20040-002',
+      address: 'Avenida Rio Branco, Rio de Janeiro - RJ',
+    ),
   ];
 
   List<AddressModel> addresses = [];
@@ -28,9 +37,25 @@ class AddressController extends ChangeNotifier {
   }
 
   Future<void> addAddress(String cep) async {
-    final novo = AddressModel(cep: cep, address: 'Endereço para o CEP $cep');
-    await _repository.saveAddress(novo);
-    await loadAddresses(); // Atualiza a lista automaticamente
+    isLoading = true;
+
+    try {
+      final fullAddress = await _viaCepService.fetchCep(cep);
+
+      if (fullAddress != null) {
+        logger.i('CEP $cep encontrado no ViaCEP: $fullAddress');
+
+        await _repository.saveAddress(fullAddress);
+        await loadAddresses();
+      } else {
+        logger.w('CEP $cep não encontrado no ViaCEP ou formato inválido.');
+      }
+    } catch (e) {
+      logger.e('Erro ao processar busca de CEP: $e');
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
   }
 
   Future<void> deleteAddress(AddressModel address) async {
